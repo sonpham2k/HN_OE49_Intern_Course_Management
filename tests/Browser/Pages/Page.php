007a6d99c0791256d5ee7aeeba9b4b61b16499d4
FT-Mystogan <viponlyc@gmail.com>
2022-03-28 07:37:41 +0700

    47659_test_view_list_course

--- tests/Browser/Pages/Page.php
+++ tests/Browser/Pages/Page.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser\Pages;
 
+use Laravel\Dusk\Browser;
 use Laravel\Dusk\Page as BasePage;
 
 abstract class Page extends BasePage
@@ -11,10 +12,18 @@ abstract class Page extends BasePage
      *
      * @return array
      */
+
     public static function siteElements()
     {
         return [
             '@element' => '#selector',
         ];
+    }
+
+    public function testView(Browser $browser, $arr = [])
+    {
+        foreach ($arr as $item) {
+            $browser->assertSee($item);
+        }
     }
 }
