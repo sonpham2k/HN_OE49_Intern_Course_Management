49e368dea47b56ccb3e445f0729688068dbe5920
ngocson <phamngocson2k2@gmail.com>
2022-04-04 22:09:36 +0700

    Logic forgot pass

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests\UserStoreRequest;
 use App\Http\Requests\ResetPassRequest;
 use App\Http\Requests\ForgotPassRequest;
+use App\Models\User;
+use App\Jobs\SendEmail;
 use App\Http\Requests\ResetPassForgotRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -61,7 +63,39 @@ class UserController extends Controller
 
     public function sendEmail(ForgotPassRequest $request)
     {
+        $count = 0;
         $email = $request->input('email');
+        $users = $this->userRepo->getAllUser();
+        foreach ($users as $user) {
+            if ($user->email == $email) {
+                $count++;
+                $trueUser = $user;
+            } else {
+                $count;
+            }
+        }
+        if ($count != 0) {
+            $characters = config('auth.randomString');
+            $charactersLength = strlen($characters);
+            $code = '';
+            for ($i = 0; $i < config('auth.randomLength'); $i++) {
+                $code .= $characters[rand(0, $charactersLength - 1)];
+            }
+            $this->userRepo->getCodeResetPass($trueUser->id, $code);
+            $message = [
+                'type' => 'Reset password',
+                'name' => $trueUser->fullname,
+                'password' => $code,
+            ];
+            SendEmail::dispatch($message, $email);
+            return redirect()
+                ->route('send.mail')
+                ->with('success', __('send success'));
+        } else {
+            return redirect()
+                ->route('send.mail')
+                ->with('error', __('send error'));
+        }
 
         return redirect()->route('send.mail');
     }
@@ -74,9 +108,43 @@ class UserController extends Controller
     public function resetForgotPass(ResetPassForgotRequest $request)
     {
         $email = $request->input('email');
+        $code = $request->input('code');
         $newpass = $request->input('newpass');
         $confirmpass = $request->input('confirmpass');
-        
+        $count = 0;
+
+        $users = $this->userRepo->getAllUser();
+        foreach ($users as $user) {
+            if ($user->email == $email) {
+                $count++;
+                $trueUser = $user;
+            } else {
+                $count;
+            }
+        }
+        if ($count == 0) {
+            return redirect()
+                ->route('reset.forgot.password')
+                ->with('error', __('none Email'));
+        } else {
+            if (!Hash::check($code, $trueUser->password)) {
+                return redirect()
+                    ->route('reset.forgot.password')
+                    ->with('error', __('wrong code'));
+            } else {
+                if ($newpass != $confirmpass) {
+                    return redirect()
+                        ->route('reset.forgot.password')
+                        ->with('error', __('error confirm'));
+                } else {
+                    $this->userRepo->resetPassAndDeleteCode($trueUser->id, $newpass);
+                    return redirect()
+                        ->route('reset.forgot.password')
+                        ->with('success', __('sucess pass'));
+                }
+            }
+        }
+
         return redirect()->route('reset.forgot.password');
     }
 
@@ -93,7 +161,7 @@ class UserController extends Controller
             'confirmpass' => $request->input('confirmpass'),
         ];
 
-        $userCheck = $this->userRepo->userCheck();
+        $userCheck = Auth::user();
         $checkSamePassOldNew = $this->userRepo->checkSamePassOldAndNew($input);
         $checkSamePassNewConfirm = $this->userRepo->checkSamePassNewAndConfirm($input);
         if (!Hash::check($input['oldpass'], $userCheck->password)) {
@@ -108,7 +176,7 @@ class UserController extends Controller
             } elseif ($checkSamePassNewConfirm) {
                 $newpassword = $this->userRepo->changePasstobcrypt($input);
                 Auth::user()->update(['password' => $newpassword]);
-                
+
                 return redirect()
                     ->route('reset')
                     ->with('success', __('sucess pass'));
